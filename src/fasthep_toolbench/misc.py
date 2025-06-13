a4759e750f75143764c22b79ce0c2dd946218cc3
kreczko <lkreczko@googlemail.com>
2025-06-13 15:50:29 +0100

    feat: add functions to print to console

--- src/fasthep_toolbench/misc.py
+++ src/fasthep_toolbench/misc.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 import hashlib
 import inspect
+from collections.abc import Callable
 from datetime import datetime
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any
 
 DEFAULT_DATE_FORMAT = "%Y.%m.%d"
 
